2ff8551930364a8e70c92e0751bc2d33c9a05732
mckrava <mckrava2@gmail.com>
2022-02-11 12:57:00 +0200

    publish branch

--- src/hooks/apollo/useApollo.tsx
+++ src/hooks/apollo/useApollo.tsx
@@ -1,80 +1,92 @@
-import { useMemo } from 'react';
-import { ApolloClient, createHttpLink, from, InMemoryCache, Resolvers,  } from '@apollo/client';
+import { useEffect, useMemo } from 'react';
+import { ApolloClient, InMemoryCache, Resolvers } from '@apollo/client';
 import { useAccountsQueryResolvers } from '../accounts/resolvers/useAccountsQueryResolvers';
 import { loader } from 'graphql.macro';
 import { useAccountsMutationResolvers } from '../accounts/resolvers/useAccountsMutationResolvers';
 import { useRefetchWithNewBlock } from '../lastBlock/useRefetchWithNewBlock';
-import { usePersistentConfig } from '../config/usePersistentConfig';
 import { useVestingMutationResolvers } from '../vesting/useVestingMutationResolvers';
 
-import { useBalanceMutationResolvers } from '../balances/useBalanceMutationResolvers';
-import { useExtensionQueryResolvers } from '../polkadotJs/useExtensionQueryResolvers';
 import { useConfigQueryResolvers } from '../config/useConfigQueryResolvers';
 import { useConfigMutationResolvers } from '../config/useConfigMutationResolver';
 import { useFeePaymentAssetsQueryResolvers } from '../feePaymentAssets/useFeePaymentAssetsQueryResolvers';
 import { usePoolsQueryResolver } from '../pools/resolvers/usePoolsQueryResolver';
-import { useBalanceQueryResolvers } from '../balances/useBalanceQueryResolvers';
+import { useBalanceQueryResolvers } from '../balances/resolvers/query/balances';
 import { useAssetsQueryResolvers } from '../assets/resolvers/useAssetsQueryResolvers';
 import { usePoolsMutationResolvers } from '../pools/resolvers/usePoolsMutationResolvers';
+import { useExtensionResolvers } from '../extension/resolvers/useExtensionResolvers';
+import { usePersistentConfig } from '../config/usePersistentConfig';
 
 /**
  * Add all local gql resolvers here
  * @returns Resolvers
  */
 export const useResolvers: () => Resolvers = () => {
-    const { Query: AccountsQueryResolver, Account } = useAccountsQueryResolvers();
-    const { Query: PoolsQueryResolver, XYKPool, LBPPool } = usePoolsQueryResolver()
-    return {
-        Query: {
-            ...AccountsQueryResolver,
-            ...useExtensionQueryResolvers(),
-            ...useConfigQueryResolvers(),
-            ...useFeePaymentAssetsQueryResolvers(),
-            ...useBalanceQueryResolvers(),
-            ...PoolsQueryResolver,
-            ...useAssetsQueryResolvers(),
-        },
-        Mutation: {
-            ...useAccountsMutationResolvers(),
-            ...useVestingMutationResolvers(),
-            ...useBalanceMutationResolvers(),
-            ...useConfigMutationResolvers(),
-            ...usePoolsMutationResolvers()
-        },
-        Account,
-        XYKPool,
-        LBPPool
-    }
+  const { Query: AccountsQueryResolver, Account } = useAccountsQueryResolvers();
+  const {
+    Query: PoolsQueryResolver,
+    XYKPool,
+    LBPPool,
+  } = usePoolsQueryResolver();
+  const { Query: ExtensionQueryResolver } = useExtensionResolvers();
+  return {
+    Query: {
+      ...AccountsQueryResolver,
+      ...ExtensionQueryResolver,
+      ...useConfigQueryResolvers(),
+      ...useFeePaymentAssetsQueryResolvers(),
+      ...useBalanceQueryResolvers(),
+      ...PoolsQueryResolver,
+      ...useAssetsQueryResolvers(),
+    },
+    Mutation: {
+      ...useAccountsMutationResolvers(),
+      ...useVestingMutationResolvers(),
+      ...useConfigMutationResolvers(),
+      ...usePoolsMutationResolvers(),
+    },
+    Account,
+    XYKPool,
+    LBPPool,
+  };
 };
 
 export const typeDefs = loader('./../../schema.graphql');
 
 /**
  * Recreates the apollo client instance each time the config changes
- * @returns 
+ * @returns
  */
 export const useConfigureApolloClient = () => {
-    const resolvers = useResolvers();
-    const cache =  new InMemoryCache();
-    // can't get the config from a query before we setup apollo
-    // therefore we get it from the local storage instead
-    const [{ processorUrl }] = usePersistentConfig();
+  const resolvers = useResolvers();
+  const cache = useMemo(() => new InMemoryCache(), []);
+  // can't get the config from a query before we setup apollo
+  // therefore we get it from the local storage instead
+  const [{ processorUrl }] = usePersistentConfig();
 
-    const client = useMemo(() => {
-        return new ApolloClient({
-            uri: processorUrl,
-            cache,
-            // TODO: don't connect in production
-            connectToDevTools: true,
-            queryDeduplication: true,
-            resolvers,
-            typeDefs,
-        })
-    }, [processorUrl]);
-    
-    useRefetchWithNewBlock(client);
-    
-    return client;
+  // todo test if url change triggers query refetch
+  const client = useMemo(() => {
+    return new ApolloClient({
+      uri: processorUrl,
+      cache,
+      // TODO: don't connect in production
+      connectToDevTools: true,
+      queryDeduplication: true,
+      resolvers,
+      typeDefs,
+    });
+    // we don't want the client to re-instantiate when the resolvers change,
+    // this is handled below in a separate effect
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [processorUrl, cache]);
+
+  useEffect(() => {
+    console.log('updating resolvers');
+    client?.setResolvers(resolvers);
+  }, [resolvers, client]);
+
+  useRefetchWithNewBlock(client);
+
+  return client;
 };
 
 export const useApollo = () => useConfigureApolloClient();
---
+++ src/hooks/extension/lib/getExtension.test.tsx
@@ -0,0 +1,45 @@
+import { Extension } from '../../../generated/graphql';
+import { getExtension } from './getExtension';
+
+export const clearMockInjectedWeb3 = () => {
+  delete (window as any).injectedWeb3;
+};
+
+export const mockInjectedWeb3 = (injected: boolean) => {
+  const injectedWeb3 = injected
+    ? {
+        'polkadot-js': {},
+      }
+    : {};
+
+  clearMockInjectedWeb3();
+
+  (window as any).injectedWeb3 = injectedWeb3;
+};
+
+describe('getExtension', () => {
+  let extension: Extension;
+
+  beforeEach(async () => {
+    clearMockInjectedWeb3();
+    extension = await getExtension();
+  });
+
+  describe('extension unavailable', () => {
+    it('should set `isAvailable = false`', () => {
+      expect(extension.isAvailable).toBe(false);
+    });
+  });
+
+  describe('extension available', () => {
+    beforeEach(async () => {
+      mockInjectedWeb3(true);
+
+      extension = await getExtension();
+    });
+
+    it('should set `isAvailable = true`', () => {
+      expect(extension.isAvailable).toBe(true);
+    });
+  });
+});
