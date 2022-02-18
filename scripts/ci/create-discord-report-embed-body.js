723cce055dca0914b8e7e418f1d6fb236f414e7c
mckrava <mckrava2@gmail.com>
2022-02-18 16:36:50 +0200

    6.13

--- scripts/ci/create-discord-report-embed-body.js
+++ scripts/ci/create-discord-report-embed-body.js
@@ -1,29 +1,9 @@
 module.exports = async ({ github, context, core }) => {
-  const { SHA, app_unit_test_percentage, app_unit_test_diff } = process.env;
+  // const { SHA, app_unit_test_percentage, app_unit_test_diff } = process.env;
   const embedBody = [
     {
-      author: {
-        name: 'Bot Tester',
-      },
       title: 'Title',
       color: '51281',
-      fields: [
-        {
-          name: 'Label',
-          value: 648,
-          inline: true,
-        },
-        {
-          name: 'Percentage',
-          value: app_unit_test_percentage,
-          inline: true,
-        },
-        {
-          name: 'Diff',
-          value: app_unit_test_diff,
-          inline: true,
-        },
-      ],
     },
   ];
 
